27cceb79e23f669e0f122eaa24e69bd17ab1d84d
mskim <mskim1@sungwoomobile.co.kr>
2019-11-20 17:41:52 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+
+const App = () => {
+    return <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+};
+
+export default App;
